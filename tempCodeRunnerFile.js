ca5cb21789b72ec1a74b8283bc080ae0a106217f
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2025-01-06 09:48:54 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+gameNum=34
+// usernum=prompt("Guess the number:");
+// while(usernum!=gameNum){
+//     usernum=prompt("Try again:");
+
+// }
+// console.log("Congrats you have guessed the right number");
