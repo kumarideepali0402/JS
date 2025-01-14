6c91f9cbacfafbd43e2dce17bf74699d09c8a7d1
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2025-01-14 13:56:50 +0530

    rock paper scissor logic building

--- TIC TAC TOE/app.js
+++ TIC TAC TOE/app.js
@@ -1,28 +1,45 @@
+const clickSound=new Audio("sounds/click.mp3");
+const winSound=new Audio("sounds/win.mp3");
+
+
 let boxes=document.querySelectorAll(".box");
 let reset=document.querySelector("#reset");
 let newGameBtn=document.querySelector('#new-game-btn');
 let msgContainer=document.querySelector('.msg-container');
 let msg=document.querySelector('.msg');
+let currentTurnElement=document.getElementById("current-turn");
 
 let turn0=true;
 
 const winPatterns=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-const showWinner = (winner) => {
-    msg.innerText=`Congrats! ${winner} won.`;
+const showWinner = (winner,winningBoxes) => {
+   
+    msg.innerText=`Congratulations!🎉 ${winner} won the game!`;
     msgContainer.classList.remove("hide");
-    
     disabledBoxes();
+    winSound.play();
+
+    for(let box of winningBoxes){
+        box.classList.add("winning-box");
+
+    }
 };
 const showDraw=()=>{
-    msg.innerText="It's a draw";
+    
+    msg.innerText="It's a draw!🤝";
     msgContainer.classList.remove("hide");
-}
+    disabledBoxes();
+
+};
 
 const resetGame=()=>{
     turn0 = true;
+    currentTurnElement.innerText='O';
     enableBoxes();
     msgContainer.classList.add("hide");
+    
+    
 
 
 };
@@ -38,7 +55,7 @@ const enableBoxes=()=>{
     for(let box of boxes){
         box.disabled=false;
         box.innerText="";
-        box.classList.remove("x","o");
+        box.classList.remove("x","o","winning-box");
 
     }
     
@@ -46,51 +63,67 @@ const enableBoxes=()=>{
 
 const checkWinner=()=>{
     for(let pattern of winPatterns){
-        pos1=boxes[pattern[0]].innerText;
-        pos2=boxes[pattern[1]].innerText;
-        pos3=boxes[pattern[2]].innerText;
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
         if(pos1!='' && pos2!='' && pos3!=''){
             if(pos1===pos2 && pos3===pos1){
                 console.log("Winner");
-                showWinner(pos1);
+                showWinner(pos1,[boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]]);
                 return true;
             }
 
         }
-        return false;
+        
        
 
     }
+    return false;
 
 };
 
 const checkDraw=()=>{
-    return Array.from(boxes).every((box)=>box.innerHTML !=="");
+    return Array.from(boxes).every((box)=>box.innerText !=="");
 };
+const toggleTurn=()=>{
+    turn0=!turn0;
+    currentTurnElement.innerText=turn0 ?'O':'X';
+}
 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
-        if(turn0){
-            box.innerHTML='O';
-            box.classList.add('o');
-            turn0=false;
-    
-        }
-        else{
-            box.innerHTML='X';
-            box.classList.add('x');
-            turn0=true;
-    
-        }
-        box.disabled=true;
-        if(!checkWinner() && checkDraw()){
-            showDraw();
+        clickSound.currentTime=0;
+        clickSound.play();
+        if(box.innerText === ""){
+
+        
+            if(turn0){
+                box.innerText='O';
+                box.classList.add('o');
+                
+        
+            }
+            else{
+                box.innerText='X';
+                box.classList.add('x');
+                
+        
+            }
+            box.disabled=true;
+            if(!checkWinner() && checkDraw()){
+                showDraw();
+            }
+            toggleTurn();
         }
 
     });
     
     
 });
+
+
+
+
 newGameBtn.addEventListener("click",resetGame);
 reset.addEventListener("click",resetGame);
 
--- practice.js
+++ practice.js
@@ -1,15 +1,28 @@
-const outermost=document.querySelector(".outermost");
-const outer=document.querySelector(".outer");
+const outermost=document.querySelector("#outermost");
+const outer=document.querySelector("#outer");
 
-const out=document.querySelector(".out");
+const out=document.querySelector("#out");
 
-outer.addEventListener('click',()=>{
-    alert("outer");
+outermost.addEventListener('click',function(e){
+    console.log(`outermost\nTarget:  ${e.target.id} ,current:  ${this.id}`);
+},true); 
+
+outer.addEventListener('click',function(e){
+    console.log(`outer\nTarget:  ${e.target.id} ,current:  ${this.id}`);
+    e.stopPropagation();
 },true);
-outermost.addEventListener('click',()=>{
-    alert("outermost");
-},true);
-out.addEventListener('click',()=>{
-    alert("out");
-},true)
 
+out.addEventListener('click',function(e){
+    console.log(`out\nTarget:  ${e.target.id} ,current:  ${this.id}`);
+},true
+)
+
+// e.stopPropogation()
+class Plant{
+    constructor(height,color){
+        this.height=height;
+        this.color=color;
+    }
+    
+}
+const rose=new Plant(30,"red");
